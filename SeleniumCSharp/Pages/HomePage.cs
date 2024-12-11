b906134fc69877cfec766be86e3ba8884722b6dc
Noemi Krysiak-Wolańska <nkrysiakwolanska@gmail.com>
2024-12-11 11:08:43 +0100

    Add tests for visibility of footer social icons and main page elements

--- SeleniumCSharp/Pages/HomePage.cs
+++ SeleniumCSharp/Pages/HomePage.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace SeleniumCSharp.FunctionalTests.Pages
 {
@@ -28,5 +29,43 @@ namespace SeleniumCSharp.FunctionalTests.Pages
             IWebElement header = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//h1[contains(text(), 'Poznaj specjalistyczną platformę szkoleniową')]")));
             return header.Text;
         }
+        public IWebElement GetLogo()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            return wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("a.MuiTypography-root.MuiTypography-inherit.MuiLink-root")));
+        }
+        public IWebElement GetMainMenu()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            return wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.MuiStack-root")));
+        }
+        public IList<IWebElement> GetMainMenuLinks()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            var menu = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.MuiStack-root")));
+            return menu.FindElements(By.CssSelector("a"));
+        }
+        public IWebElement GetFooter()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            return wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.MuiGrid-container.css-1xorlg6")));
+        }
+        public IReadOnlyCollection<IWebElement> GetFooterLinks()
+        {
+            return _driver.FindElements(By.CssSelector("footer a"));
+        }
+        public IList<IWebElement> GetFooterSocialIcons()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            var socialIconsContainer = wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".MuiBox-root.css-1a88wtd")));
+
+            return socialIconsContainer.FindElements(By.CssSelector("a"));
+        }
+
+
+
+
     }
 }
+     
+    
